94ea7dbd25e1695b9a26e1ca24b54235d533f738
Rahul <thecoderrahul@gmail.com>
2022-12-27 13:28:25 +0530

    Bugs fixed

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Build and Deploy Your Own ChatGPT AI Application That Will Help You Code
-![Open AI CodeGPT](https://i.ibb.co/LS4DRhb/image-257.png)
+Preview:
+![ZovoAI Preview](./preview.png')
 
-### Launch your development career with project-based coaching - https://www.jsmastery.pro
+Ref: JSM
--- client/script.js
+++ client/script.js
@@ -72,7 +72,7 @@ const handleSubmit = async (e) => {
     const messageDiv = document.getElementById(uniqueId) //Selecting the bot's div using specific div ID
     loader(messageDiv) //Stating the loader 
 
-    const response = await fetch('http://localhost:5000', {
+    const response = await fetch('http://localhost:5000/', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
